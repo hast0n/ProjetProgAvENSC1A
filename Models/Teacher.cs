c4983ddbe8152b80dd147386e996209360c35aaa
hast0n <devreese.martin@gmail.com>
2020-05-07 16:41:23 +0200

    added Courses property to Teacher class

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using ProjetProgAvENSC1A.Services;
 
 namespace ProjetProgAvENSC1A.Models
 {
     class Teacher : Person
     {
-        public void CoursesTaught()
+        // Get all courses dispensed by this teacher
+        public List<EntryType> Courses => App.DB[DBTable.Courses].Entries.Where(entry =>
         {
-            //work on the database
-        }
+            Course c = (Course) entry;
+            return c.Teachers.Contains(this);
+        }).ToList();
     }
 }
