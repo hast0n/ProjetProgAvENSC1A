13dcd5cfb41419de73430348afc04ab81975a53d
hast0n <devreese.martin@gmail.com>
2020-05-14 10:29:18 +0200

    added comments

--- Services/IDataTable.cs
+++ Services/IDataTable.cs
@@ -10,23 +10,65 @@ namespace ProjetProgAvENSC1A.Services
 {
     interface IDataTable
     {
+        /// <summary>
+        /// Get the entries of the table.
+        /// </summary>
         public List<EntryType> Entries { get; }
 
+        /// <summary>
+        /// Get an entry using its index.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
         public EntryType this [int index] => Entries[index];
+
+        /// <summary>
+        /// Get an entry using its UUID.
+        /// </summary>
+        /// <param name="uuid"></param>
+        /// <returns></returns>
         public EntryType this [string uuid] => Entries.Find(e => e.UUID.Equals(uuid));
 
+        /// <summary>
+        /// Append an entry to the table.
+        /// </summary>
+        /// <param name="entry"></param>
+        /// <returns></returns>
         public bool AddEntry(EntryType entry);
 
+        /// <summary>
+        /// Update an entry value in the table.
+        /// </summary>
+        /// <param name="oldEntry"></param>
+        /// <param name="newEntry"></param>
+        /// <returns></returns>
         public bool UpdateEntry(EntryType oldEntry, EntryType newEntry);
 
+        /// <summary>
+        /// Remove an entry from the table.
+        /// </summary>
+        /// <param name="uuid"></param>
+        /// <returns></returns>
         public bool RemoveEntry(string uuid);
 
         // use of asynchronous json serializing process
         // -> switch to async method
+        /// <summary>
+        /// Load data Json files from storage.
+        /// </summary>
+        /// <returns></returns>
         public Task<bool> LoadFromStorage();
 
+        /// <summary>
+        /// Write data to Json files in storage
+        /// </summary>
+        /// <returns></returns>
         public Task<bool> WriteToStorage();
 
+        /// <summary>
+        /// Wipe the content of the table
+        /// </summary>
+        /// <returns></returns>
         public bool DropContent();
     }
 }
